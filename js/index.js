2ceb35fee9bfe854a7fea3f0c4530f7422c3014e
abrand <351783080@qq.com>
2017-04-26 11:54:28 +0800

    xx

---
+++ js/index.js
@@ -0,0 +1,28 @@
+var work1 = document.getElementById("work1") ;
+var work2 = document.getElementById("work2") ;
+var img = document.getElementById("preview");
+
+
+
+work2.onmousemove = function function_name(argument) {
+	img.src =  "img/work-2.png";
+	//console.log(1);
+}
+
+work4.onmousemove = function function_name(argument) {
+	img.src =  "img/work-4.png";
+	//console.log(1);
+}
+
+work2.onclick = function function_name(argument) {
+	window.open("work-2.html");
+	console.log(1);
+}
+
+work4.onclick = function function_name(argument) {
+	window.open("work-4.html");
+	console.log(1);
+}
+
+
+
