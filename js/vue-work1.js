14c7e50cf543b25f04fa6faa7270b3e72087ca9f
abrand <351783080@qq.com>
2017-05-21 19:18:37 +0800

    xx

--- js/vue-work1.js
+++ js/vue-work1.js
@@ -1,53 +1,5 @@
 
 
-// var app2 = new Vue({
-//   el: '#app-2',
-//   data: {
-//     message: '页面加载于 ' + new Date()
-//   }
-// })
-
-// var app3 = new Vue({
-//   el: '#app-3',
-//   data: {
-//     seen: false
-//   }
-// })
-
-
-// var category_hot_list_display = new Vue({
-//     el: ".xxx",
-//     data: {
-//         qqq: [
-//             {img_src: "img/head.png"},
-//             {img_src: "img/shouye.png"}
-//         ]
-//     }
-// });
-
-// var app4 = new Vue({
-//   el: '#app-4',
-//   data: {
-//     todos: [
-//       { text: '首页' },
-//       { text: '动画' },
-//       { text: '番剧' },
-//       { text: '国创' },
-//       { text: '音乐' },
-//       { text: '舞蹈' },
-//       { text: '游戏' },
-//       { text: '科技' },
-//       { text: '生活' },
-//       { text: '鬼畜' },
-//       { text: '时尚' },
-//       { text: '广告' },
-//       { text: '娱乐' },
-//       { text: '影视' }
-//     ]
-//   }
-// })
-
-
 var contianer1 = new Vue({
 	el: '.contianer-1',
 	data: {
--- js/work1.js
+++ js/work1.js
@@ -1,1 +1,4 @@
 $("li").eq(11).css("border-left-width","1px");
+$("li").eq(0).css("background-color","white");
+$("li").eq(0).css("border-left-width","1px");
+$("li").eq(0).css("border-bottom-width","0px");
